eced10210b30f9b412a12f572ff9406fb4b61db6
Gal Koren <gal.koren@kenshoo.com>
2020-09-01 17:52:59 +0300

    exercise 5 unsolved

---
+++ src/main/java/pl/workshop/device/DeviceTable.java
@@ -0,0 +1,21 @@
+package pl.workshop.device;
+
+import com.kenshoo.jooq.AbstractDataTable;
+import org.jooq.Record;
+import org.jooq.TableField;
+import org.jooq.impl.SQLDataType;
+import pl.workshop.campaign.CampaignTable;
+
+public class DeviceTable extends AbstractDataTable<DeviceTable> {
+
+    public final static DeviceTable INSTANCE = new DeviceTable("devices");
+
+    DeviceTable(String tableName) { super(tableName); }
+    DeviceTable(DeviceTable aliased, String alias) { super(aliased, alias); }
+    @Override public DeviceTable as(String alias) { return new DeviceTable(this, alias); }
+
+
+    public final static TableField<Record, Integer> campaign_id = INSTANCE.createFKField("campaign_id", CampaignTable.id);
+    public final static TableField<Record, String> device = INSTANCE.createField("device", SQLDataType.VARCHAR(20));
+
+}
---
+++ src/main/java/pl/workshop/device/DeviceType.java
@@ -0,0 +1,7 @@
+package pl.workshop.device;
+
+public enum DeviceType {
+    DESKTOP,
+    MOBILE,
+    TABLET
+}
